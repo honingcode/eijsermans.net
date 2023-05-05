5db8a4f5329a79d0efedf1b024322e2b3e3c3799
honingcode <levihoning@gmail.com>
2023-05-05 15:24:13 +0800

    title

--- merken.js
+++ merken.js
@@ -37,7 +37,7 @@ for (let i = 0; i < alleMerkContainer.length; i++) {
 
 
 for (let i = 0; i < alleMerkContainer.length; i++) {
-    alleMerkContainer[i].addEventListener('touchleave', function() {
+    alleMerkContainer[i].addEventListener('touchend', function() {
         merkBeschrijving[i].classList.remove('display-merk-beschrijving')
     })
 }
