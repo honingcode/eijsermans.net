c9d991837599c2adbe962e180f60cee3728c07aa
honingcode <levihoning@gmail.com>
2023-05-09 11:31:18 +0800

    over-ons

--- overons.js
+++ overons.js
@@ -1,15 +1,22 @@
 let paths = document.querySelectorAll('path');
 
-let overOnsSection = document.getElementById('section-goes-over-ons')
+
+const element = document.getElementById('section-goes-over-ons');
+const elementTop = element.getBoundingClientRect().top + window.scrollY;
+const elementHeight = element.clientHeight;
+const windowHeight = window.innerHeight;
 
 
 fillSvgPaths()
 
 document.addEventListener('scroll', fillSvgPaths)
 
+
 function fillSvgPaths() {
 
-    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    const scrollTop = window.scrollY;
+    const scrollPercentage = Math.max(0, Math.min(1, (scrollTop - elementTop) / (elementHeight - windowHeight)));
+    console.log(scrollPercentage)
 
     for (var i = 0; i < paths.length; i++) {
         let path = paths[i];
@@ -25,3 +32,10 @@ function fillSvgPaths() {
     }
 }
 
+
+
+
+
+
+
+// let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
