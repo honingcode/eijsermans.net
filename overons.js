6bf6169e75013035867acf56dc2f0ea5b9c85413
honingcode <levihoning@gmail.com>
2023-05-09 08:40:17 +0800

    over-ons

---
+++ overons.js
@@ -0,0 +1,27 @@
+let paths = document.querySelectorAll('path');
+
+let overOnsSection = document.getElementById('section-goes-over-ons')
+
+
+fillSvgPaths()
+
+document.addEventListener('scroll', fillSvgPaths)
+
+function fillSvgPaths() {
+
+    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    for (var i = 0; i < paths.length; i++) {
+        let path = paths[i];
+
+        let pathLength = path.getTotalLength();
+
+        path.style.strokeDasharray = pathLength;
+        path.style.strokeDashoffset = pathLength;
+
+        let drawLength = pathLength * scrollPercentage;
+
+        path.style.strokeDashoffset = pathLength - drawLength;
+    }
+}
+
